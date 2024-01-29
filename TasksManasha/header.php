9e86c95f7cbf0aca40bb9a23e0fe0fd42462e883
manasha23000 <manasha23000@student.hamk.fi>
2024-01-29 12:02:42 +0200

    Ex2 final

--- TasksManasha/header.php
+++ TasksManasha/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title ?> </title>
+    <title>   <?php echo $title ?>   </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
@@ -12,8 +12,8 @@
     <div class="Container">
         <header>
         
-            <div class="row">
-                <div class="col-md-4">
+            <div class="row nav-bacground">
+                <div class="col-md-4 ">
                      <h2>Manasha Siriwardana</h2>
                 </div>   
 
